d53d9fd941b1d04bd181104458324b02d9579211
Laura Mattson <lauramattsons@gmail.com>
2026-03-03 00:13:23 +0000

    Skapa början av filen

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="sv">
+
+<head>
+    <meta charset="utf-8">
+    <title>EcoVibe</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <!-- Navigation -->
+    <header>
+        <nav class="navbar navbar-expand-md bg-white navbar-light border-bottom p-3">
+            <div class="container">
+                <a href="index-php" class="navbar-brand">
+                    <i class="fa-solid fa-clover text-success"></i> <!-- ändrar färg -->
+                    <span>EcoVibe</span>
+                </a>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a href="#" class="nav-link active">Hem</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Tjänster</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Om oss</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Kontakt</a></li>
+                </ul>
+                <form class="d-flex">
+                    <input type="search" class="form-control me-2" placeholder="Sök...">
+                    <button type="submit" class="btn btn-success">Sök</button>
+                </form>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <!-- Hero Section / JUmbotron -->
+        <section class="hero text-center text-white py-5"> <!-- py = utrymme upp och ner -->
+            <div class="container py-5">
+                <h1 class="display-4 fw-bold">
+                    <i class="fa-solid fa-seedling"></i>
+                    Bygg en grönare framtid.
+                </h1>
+                <p class="lead mt-3">
+                    Vi hjälper företag och privatpersoner att minska sin miljöpåverkan genom hållbara lösningar och innovativa tjänster.
+                </p>
+                <a href="#" class="btn btn-light btn-lg text-success rounded-pill mt-2">
+                    Kom igång <i class="fa-solid fa-arrow-right ms-2"></i>
+                </a>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
